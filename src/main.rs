f83e09634e6d099facbc37b2ee15ec4710cab8ab
Daniel Buse <daniel.buse@giga-infosystems.com>
2018-06-29 11:50:03 +0200

    use net2 with quick and dirty println logging

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,11 @@ fn main() {
     let host = url.host_str().expect("URL must have host");
     let port = url.port().unwrap_or(80);
 
-    loop {
-        let mut line = String::new();
-        ::std::io::stdin()
-            .read_line(&mut line)
-            .expect("correct input");
-        let client = httpbis::Client::new_plain(host, port, Default::default()).expect("client");
+    let client = httpbis::Client::new_plain(host, port, Default::default()).expect("client");
 
-        let resp = client
-            .start_get(url.path(), host)
-            .collect()
-            .wait()
-            .expect("execute request");
-
-        print!("{}", resp.dump());
-    }
+    let _resp = client
+        .start_get(url.path(), host)
+        .collect()
+        .wait()
+        .expect("execute request");
 }
